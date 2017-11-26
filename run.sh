081a034e0f098085369f21508ec8ba2b36f76eef
egee-irl <brian@egee.io>
2017-11-25 22:00:37 -0500

    fucking git

---
+++ run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/home/rust/rustserver/RustDedicated +server.ip 0.0.0.0 +server.port 28015 +server.tickrate 10 +server.hostname "egee.io" +server.identity "rust_dir" +server.seed 793197 +server.maxplayers 10 +server.worldsize 1500 +server.saveinterval 600 +rcon.ip 0.0.0.0 +rcon.port 28016 -logfile rust.log
+
