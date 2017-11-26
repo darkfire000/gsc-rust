98eb392de1db3672b8611b650ecd56c9df0a53c8
egee-irl <brian@egee.io>
2017-11-26 13:02:18 -0500

    more stuff

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,27 @@
 FROM fedora
-RUN dnf install -y glibc.i686 libstdc++.i686
 
+RUN dnf install -y glibc.i686 libstdc++.i686
 RUN adduser rust
 RUN chown rust /home/rust
-USER rust
 
+USER rust
 RUN mkdir /home/rust/steamcmd
 WORKDIR /home/rust/steamcmd
 RUN curl -sqL "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar zvxf -
 RUN ./steamcmd.sh +login anonymous +force_install_dir /home/rust/rustserver +app_update 258550 validate +quit
-ADD run.sh /home/rust/rustserver/run.sh
+
 USER root
-RUN chown rust /home/rust/rustserver/run.sh
+
+ARG server_id=chilling
+ENV SERVER_ID=${server_id}
+
+ADD run.sh /home/rust/rustserver/run.sh
+ADD bans.cfg /home/rust/rustserver/server/${server_id}/cfg/bans.cfg
+ADD server.cfg /home/rust/rustserver/server/${server_id}/cfg/server.cfg
+ADD users.cfg /home/rust/rustserver/server/${server_id}/cfg/users.cfg
+RUN chown rust:rust /home/rust/rustserver/run.sh
+RUN chown -R rust:rust /home/rust/rustserver/server/${server_id}/
+
 USER rust
 WORKDIR /home/rust/rustserver
 
--- run.sh
+++ run.sh
@@ -1,18 +1,21 @@
 #!/bin/bash
+
+# This export is required because the version of Steamworks.NET that Rust ships wish is broken
 export LD_LIBRARY_PATH=/home/rust/rustserver:/home/rust/rustserver/RustDedicated_Data:{$LD_LIBRARY_PATH};
 
+#/home/rust/rustserver/server/test/cfg
+
 ip="0.0.0.0"
-port="28011"
-rconport="28012"
+port="28015"
+rconport="28015"
 rconpassword="_CHANGEME_"
-rconweb="1" # Value is: 1 for Facepunch's web panel; 0 for RCON tools like Rusty or Rustadmin
-servername="UrFace"
+rconweb="1"
+servername="egeeio"
+serverid=$SERVER_ID
 maxplayers="2"
+seed=""
+worldsize="2000"
+saveinterval="300"
+tickrate="30"
 
-# Advanced Start Settings
-seed="" #  default random; range : 1 to 2147483647 ; used to change or reproduce a procedural map
-worldsize="2000" # default 3000; range : 1000 to 6000 ; map size in meters
-saveinterval="300" # Auto-save in seconds
-tickrate="30" # default 30; range : 15 to 100
-
-/home/rust/rustserver/RustDedicated -batchmode +server.ip ${ip} +server.port ${port} +server.tickrate ${tickrate} +server.hostname ${servername} +server.identity test  +server.maxplayers ${maxplayers} +server.worldsize ${worldsize} +server.saveinterval ${saveinterval} +rcon.web ${rconweb} +rcon.ip ${ip} +rcon.port ${rconport} +rcon.password ${rconpassword} -logfile buttface.log
+/home/rust/rustserver/RustDedicated -batchmode +server.ip ${ip} +server.port ${port} +server.tickrate ${tickrate} +server.hostname ${servername} +server.identity ${SERVER_ID} +server.maxplayers ${maxplayers} +server.worldsize ${worldsize} +server.saveinterval ${saveinterval} +rcon.web ${rconweb} +rcon.ip ${ip} +rcon.port ${rconport} +rcon.password ${rconpassword} -logfile buttface.log
