6f10dc97c444a96b3c428db42dc804f44821aeec
egee-irl <brian@egee.io>
2017-11-30 11:55:31 -0800

    Rust and its shitty documentation

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # This export is required because the version of Steamworks.NET that Rust ships wish is broken
 export LD_LIBRARY_PATH=/home/rust/rustserver:/home/rust/rustserver/RustDedicated_Data:{$LD_LIBRARY_PATH};
 
-/home/rust/rustserver/RustDedicated -batchmode +server.ip ${IP} +server.port ${PORT} +server.tickrate ${TICKRATE} +server.hostname ${SERVERNAME} +server.identity ${SERVER_ID} +server.maxplayers ${MAXPLAYERS} +server.worldsize ${WORLDSIZE} +server.saveinterval ${SAVEINTERVAL} +rcon.web ${RCONWEB} +rcon.ip ${IP} +rcon.port ${RCONPORT} +rcon.password ${RCONPASSWORD} -logfile buttface.log
+/home/rust/rustserver/RustDedicated -batchmode +server.ip ${IP} +server.seed ${SEED} +server.port ${PORT} +server.tickrate ${TICKRATE} +server.hostname ${SERVERNAME} +server.identity ${SERVER_ID} +server.maxplayers ${MAXPLAYERS} +server.worldsize ${WORLDSIZE} +server.saveinterval ${SAVEINTERVAL} +rcon.web ${RCONWEB} +rcon.ip ${IP} +rcon.port ${RCONPORT} +rcon.password ${RCONPASSWORD} -logfile buttface.log
