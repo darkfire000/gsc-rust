c53fe54d9b045c1767b374290382ba8f6900b61f
egee-irl <brian@egee.io>
2018-06-04 16:37:40 -0700

    added required files

--- run.sh
+++ run.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-touch /opt/rustserver/server.log
-/opt/rustserver/RustDedicated \
+TEMP_DIR="/home/gsc/rustserver"
+WORK_DIR="/home/gsc/server_files"
+echo "before"
+if [ -d "$TEMP_DIR" ]; then
+  echo "Beginning initial bootstrap, please wait..."
+  cp -r $TEMP_DIR/* $WORK_DIR
+  rm -rf $TEMP_DIR
+  echo "Bootstrap finished, launching server..."
+fi
+
+$WORK_DIR/RustDedicated \
   +server.ip ${IP} \
   +server.port ${PORT} \
   +server.tickrate ${TICKRATE} \
@@ -11,4 +20,4 @@ touch /opt/rustserver/server.log
   +rcon.port ${RCONPORT} \
   +rcon.password ${RCONPASSWORD} \
   -logfile server.log &
-tail -f /opt/rustserver/server.log
+tail -f $WORK_DIR/server.log
