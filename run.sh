096b5c1a72e495c2c2cedd8470b9d0e7cb4ef6fa
egee-irl <brian@egee.io>
2018-06-09 20:01:46 -0700

    hm this just might work

--- run.sh
+++ run.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
-touch /home/gsc/rustserver/server.log
-/home/gsc/rustserver/RustDedicated \
+TEMP_DIR="/home/gsc/rustserver"
+WORK_DIR="/home/gsc/server_files"
+if [ -d "$TEMP_DIR" ]; then
+  echo "Beginning initial bootstrap, please wait..."
+  cp -r $TEMP_DIR/* $WORK_DIR
+  # rm -rf $TEMP_DIR
+  echo "Bootstrap finished, launching server..."
+fi
+touch /home/gsc/server_files/server.log
+/home/gsc/server_files/RustDedicated \
   +server.ip ${IP} \
   +server.port ${PORT} \
   +server.tickrate ${TICKRATE} \
@@ -11,4 +19,4 @@ touch /home/gsc/rustserver/server.log
   +rcon.port ${RCONPORT} \
   +rcon.password ${RCONPASSWORD} \
   -logfile server.log &
-tail -f /home/gsc/rustserver/server.log
+tail -f /home/gsc/server_files/server.log
